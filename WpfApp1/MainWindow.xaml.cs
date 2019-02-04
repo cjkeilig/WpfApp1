ba85e36182ac51308d7d9526134010f05c04cd8b
UNO User <unouser@unomaha.edu>
2019-02-04 00:31:59 -0600

    Add project files.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public PersonList Persons { get; set; }
+
+        public MainWindow()
+        {
+
+            InitializeComponent();
+            Persons = PersonList.GetSampleData();
+
+            //this.ItemsControl1.ItemsSource = Persons;
+
+            
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //ContentPresenter contentPresenter = this.ItemsControl1.ItemContainerGenerator.ContainerFromIndex(Int32.Parse(ScrollToIndex.Text)) as ContentPresenter;
+            //contentPresenter.BringIntoView();
+        }
+    }
+}
---
+++ WpfApp1/Person.cs
@@ -0,0 +1,86 @@
+﻿using Csla;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    [Serializable]
+    public class Person : Csla.BusinessBase<Person>
+    {
+        public static readonly PropertyInfo<String> PropertyName = RegisterProperty<String>(c => c.Name);
+        public string Name
+        {
+            get
+            {
+                return GetProperty(PropertyName);
+            }
+            set
+            {
+                SetProperty(PropertyName, value);
+            }
+        }
+
+        public static readonly PropertyInfo<String> PropertyDescription = RegisterProperty<String>(c => c.Description);
+        public string Description
+        {
+            get
+            {
+                return GetProperty(PropertyDescription);
+            }
+            set
+            {
+                SetProperty(PropertyDescription, value);
+            }
+        }
+
+
+        internal void Child_Fetch(PersonDto personDto)
+        {
+            LoadProperty(PropertyName, personDto.Name);
+            LoadProperty(PropertyDescription, personDto.Description);
+        }
+
+
+        
+    }
+
+    [Serializable]
+    public class PersonList : Csla.BusinessListBase<PersonList, Person>
+    {
+        internal void Child_Fetch()
+        {
+            RaiseListChangedEvents = false;
+
+            for (int i = 0; i < 100; i++)
+            {
+                var personOne = DataPortal.FetchChild<Person>(new PersonDto() { Name = "Test 1", Description = "Test 1 Description" });
+                var personTwo = DataPortal.FetchChild<Person>(new PersonDto() { Name = "Test 2", Description = "Test 2 Description" });
+
+                Add(personOne);
+                Add(personTwo);
+            }
+
+
+
+
+            RaiseListChangedEvents = true;
+
+        }
+
+
+
+        public static PersonList GetSampleData()
+        {
+            return DataPortal.FetchChild<PersonList>();
+        }
+    }
+
+    public class PersonDto
+    {
+        public string Name;
+        public string Description;
+    }
+}
