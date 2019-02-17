826846ac6818d41d4dc079e9db89e1b495ab686b
UNO IST Support <uno-ist-support@unomaha.edu>
2019-02-16 21:17:09 -0600

    Add a new Expandable Group Box control

---
+++ WpfApp1/Controls/ExpandCollapseGroupBox.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace WpfApp1.Controls
+{
+    public class ExpandCollapseGroupBox : GroupBox
+    {
+
+
+        public bool? IsChecked
+        {
+            get { return (bool?)this.GetValue(StateProperty); }
+            set { this.SetValue(StateProperty, value); }
+        }
+
+        public static readonly DependencyProperty StateProperty = DependencyProperty.Register(
+          "IsChecked", typeof(bool?), typeof(ExpandCollapseGroupBox), new PropertyMetadata(true, OnCheckPropertyChanged));
+
+        //static ExpandCollapseGroupBox()
+        //{
+        //    May need this to fix have to put the default style in the App.xaml, it is supposed to go in the Themes/Generic.xaml file
+        //    DefaultStyleKeyProperty.OverrideMetadata(typeof(ExpandCollapseGroupBox), new FrameworkPropertyMetadata(typeof(ExpandCollapseGroupBox)));
+        //}
+
+        private static void OnCheckPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            bool isChecked = e.NewValue as bool? ?? false;
+
+            var visibility = isChecked ? Visibility.Visible : Visibility.Collapsed;
+
+            var childCount = LogicalTreeHelper.GetChildren(d);
+
+            foreach (var child in childCount)
+            {
+                if (child is UIElement uiElement)
+                {
+                    uiElement.Visibility = visibility;
+                }
+            }
+        }
+    }
+}
---
+++ WpfApp1/Converters/ExpandCollapseImageConverter.cs
@@ -0,0 +1,36 @@
+﻿using FontAwesome.WPF;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+
+namespace WpfApp1.Converters
+{
+    public class ExpandCollapseImageConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+
+            // ChevronUp and ChevronDown are the IDs of the font awesome icons
+            var chevronUp = values[0] as Enum;
+            var chevronDown = values[1] as Enum;
+
+            var isChecked = values[2] as bool?;
+            var droppedDown = isChecked ?? false;
+
+            if (droppedDown)
+                return chevronUp;
+            else
+                return chevronDown;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
